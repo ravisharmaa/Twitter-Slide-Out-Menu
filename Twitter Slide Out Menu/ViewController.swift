d75094d603a20a2187c99850b83bb8d6a9229338
joelangenderfer <joelangen@me.com>
2019-01-30 19:39:52 -0500

    Initial Commit

---
+++ Twitter Slide Out Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter Slide Out Menu
+//
+//  Created by Joe Langenderfer on 1/30/19.
+//  Copyright © 2019 Joe Langenderfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
